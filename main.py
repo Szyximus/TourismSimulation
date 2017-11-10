2f6c7d17b0f82d02f2e7ebc4152810ceae211125
Szyx <sjakobczyk@gmail.com>
2017-11-10 01:15:26 +0100

    gitignore

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Window(pyglet.window.Window):
         super().__init__(resizable=True, caption='Tourism Simulation', visible= False)
         self.set_minimum_size(226, 354)
         self.set_maximum_size(2260, 3540)
+        self.frame_rate = 1/60.0
         self.set_visible(True)
         self.krakow_map = pyglet.image.load('graphics\Krk.png')
         self.krakow_map.anchor_x = self.krakow_map.width // 2
@@ -46,4 +47,5 @@ class Window(pyglet.window.Window):
 if __name__ == '__main__':
 
     window = Window()
+    #pyglet.clock.schedule_interval(window.on_draw(), window.frame_rate)
     pyglet.app.run()
