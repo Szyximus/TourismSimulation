71200854d18994b3a09b26313dbc8e9a0fa8b5d0
Szyx <sjakobczyk@gmail.com>
2017-11-29 21:39:58 +0100

    Basic POIs labeling

--- src/point_of_interest.py
+++ src/point_of_interest.py
@@ -1,18 +1,35 @@
 import pyglet
+from poilabel import PoiLabel
 
 
 class PointOfInterest:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y,name, attractiveness, price, time_needed):
         self.x = x
         self.y = y
+        self.name = name
+        self.attractiveness = attractiveness
+        self.price = price
+        self.time_needed = time_needed
 
         self.img = pyglet.image.load('./graphics/POI.png')
         self.img.anchor_x = self.img.width // 2
         self.img.anchor_y = self.img.height // 2
         self.sprite = pyglet.sprite.Sprite(self.img, x=self.x, y=self.y)
 
+        self.label = PoiLabel(name, x, y)
+
     def draw(self, windowx, windowy):
         self.sprite.x = windowx + self.x
         self.sprite.y = windowy + self.y
         self.sprite.draw()
+        self.label.draw(self.sprite.x, self.sprite.y)
+
+
+
+
+
+
+
+
+
--- src/simulation.py
+++ src/simulation.py
@@ -9,11 +9,12 @@ class Simulation:
     def __init__(self):
         self.agents = []
         self.spawn_points = [SpawnPoint(-990, 1300, 1)]
-        self.pois = [PointOfInterest(-680 , 1090), PointOfInterest(-480, 990), PointOfInterest(-310, 980)]
+        self.pois = [PointOfInterest(-680, 1090, 'McDonalds', 5, 2, 4), PointOfInterest(-480, 990, 'Sukiennice', 7, 0, 2), PointOfInterest(-310, 980, 'Kościół Mariacki', 3, 0, 8)]
 
     def update(self, dt):
         list(map(lambda spawn_point: spawn_point.update(self.agents), self.spawn_points))
         list(map(lambda agent: agent.update(dt), self.agents))
+        #list(map(lambda poi: poi.update(), self.pois))
 
     def draw(self, windowx, windowy):
         list(map(lambda spawn: spawn.draw(windowx, windowy), self.spawn_points))
