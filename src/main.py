96b50d2bf330498f91b71ab2bb81aa698121f488
Paweł Płatek <e2.8a.95@gmail.com>
2017-12-16 13:21:36 +0100

    start fixing time

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ class Window(pyglet.window.Window):
         super().__init__(resizable=True, caption='Tourism Simulation', visible=False)
         self.set_minimum_size(640, 480)
         self.set_maximum_size(2260, 3540)
-        self.frame_rate = 1/60.0
+        self.frame_rate = 0.1
 
         self.map = Map(self.width, self.height)
         self.set_visible(True)
@@ -52,6 +52,5 @@ class Window(pyglet.window.Window):
 if __name__ == '__main__':
 
     window = Window()
-
     pyglet.clock.schedule_interval(window.update, window.frame_rate)
     pyglet.app.run()
--- src/simulation.py
+++ src/simulation.py
@@ -3,11 +3,13 @@ from agent import Agent
 from spawn_point import SpawnPoint
 from point_of_interest import PointOfInterest
 
+from math import floor
 import numpy as np
 from PIL import Image
 
 
 class Simulation:
+    DEBUG = True
 
     def __init__(self, size_x, size_y):
         self.agents = []
@@ -29,21 +31,53 @@ class Simulation:
                      PointOfInterest(-585, 955, 'Wieża Ratuszowa', 2, 1, 3, None)]
 
         self.size_x, self.size_y = size_x, size_y
-        self.map_raster = None
-        self.prepare_map_raster()
+        self.grid = None
+        self.prepare_grid()
 
+        # one meter is 1.5 pixels
         self.pixels_per_meter = 1.5
 
-    def prepare_map_raster(self):
+        # time speed multiplier. 2 means that one second in real is two seconds in simulation
+        self.time_speed = 5
+
+        # how often (in simulation time) update will take place
+        self.time_density = 1
+        self.simulation_delta_time = 0
+        self.real_time = 0
+
+        self.timestamp = 1513426631.0
+
+        # how much grid is smaller than map
+        # not used, probably won't help efficiency
+        self.grid_scale = 1
+
+    def prepare_grid(self):
         krakow_map_gray = Image.open('./graphics/Navigation.png')
         krakow_map_gray = krakow_map_gray.convert('L')
-        self.map_raster = np.flip(np.array(krakow_map_gray), 0)
+        self.grid = np.array(krakow_map_gray)
         # PIL (0,0) is in upper left corner, pyglet and our map_raster in bottom left corner
         # after np.flip it will be consistent
+        self.grid = np.flip(self.grid, 0)
+
+    def get_grid(self, y, x):
+        return self.grid[floor(y * self.grid_scale)][floor(x * self.grid_scale)]
+
+    def set_grid(self, y, x, v):
+        self.grid[floor(y * self.grid_scale)][floor(x * self.grid_scale)] = v
 
     def update(self, dt):
-        list(map(lambda spawn_point: spawn_point.update(self.agents, self), self.spawn_points))
-        list(map(lambda agent: agent.update(dt), self.agents))
+        self.simulation_delta_time += dt * self.time_speed
+        self.real_time += dt
+
+        if self.simulation_delta_time >= self.time_density:
+            if Simulation.DEBUG:
+                print("Real time:", round(self.real_time, 2),
+                      "  |  Simulation time:", round(self.real_time * self.time_speed, 2),
+                      "  |  Time delta: ", round(self.simulation_delta_time, 2))
+            self.timestamp += self.simulation_delta_time
+            list(map(lambda spawn_point: spawn_point.update(self.simulation_delta_time, self), self.spawn_points))
+            list(map(lambda agent: agent.update(self.simulation_delta_time), self.agents))
+            self.simulation_delta_time = 0
 
     def draw(self, windowx, windowy):
         list(map(lambda agent: agent.draw(windowx, windowy), self.agents))
